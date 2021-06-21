b96c63b86bc4069ff7fee3091240997fdc34a229
YuyusanK <kurinoyuichi@gmail.com>
2021-06-20 17:39:07 -0700

    fix: removed all unnecessary console.log()

--- script/reservations.js
+++ script/reservations.js
@@ -3,13 +3,11 @@ const spotsModel = require('../database/models/spotsModel');
 const stripe = require('../services/stripe/payment');
 async function startScript(){
     schedule.scheduleJob('*/5 * * * * *', async function() {
-        console.log("startScript\n")
         try {
             const tickets = await spotsModel.getOutstandingReservations(); // gets outstanding reservations
             // Charge all tickets
             tickets.map(async ticket => {
                 const chargeId = await stripe.authorizeByCustomerAndSource(400, 'Inactive violation', ticket.stripe_customer_id, ticket.card_id);
-                console.log(chargeId);
                 const status = await spotsModel.cancelReservationAndUpdateSpot(chargeId.id, chargeId.amount, ticket.id, ticket.spot_hash);
             });
 
