4407a473765e69b23df7b52f2f9b0e79c1d06b5e
YuyusanK <kurinoyuichi@gmail.com>
2021-06-20 17:36:17 -0700

    fix: added updates to total_price and is_paid when canceling reservation

--- database/models/spotsModel.js
+++ database/models/spotsModel.js
@@ -1,21 +1,21 @@
 const db = require('../dbConfig');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 /**
  * update dead lots with their new information
  * @param
  * @returns {Promise<{status: string}>}
  */
-async function cancelOutstandingReservations() {
-    const result = {status: 'failed'};
-    const reservation_time_limit = new Date();
-    reservation_time_limit.setHours(reservation_time_limit.getHours() - 48);
+async function cancelReservationAndUpdateSpot(stripe_charge_id, total_price, id, secret) {
+    let result = {status: 'failed'};
     await db.transaction(async (transaction) => {
-        console.log(reservation_time_limit+"\n"+updateJson)
         try {
             await db('reservations')
-                .where('reserved_at', '<', reservation_time_limit)
-                .update(updateJson)
+                .where({id})
+                .update({status: 'CANCELED', stripe_charge_id, total_price, is_paid:true})
                 .transacting(transaction);
+
+            await db('spots')
+                .where({secret})
+                .update({spot_status: 'UNOCCUPIED'});
             result.status = 'success';
             await transaction.commit();
         } catch (err) {
@@ -32,39 +32,18 @@ async function cancelOutstandingReservations() {
  * @param
  * @returns {Promise<{status: string}>}
  */
-async function checkOutstandingReservations() {
-    const result = {status: 'failed'};
+async function getOutstandingReservations() {
     const reservation_time_limit = new Date();
     reservation_time_limit.setHours(reservation_time_limit.getHours() - 48);
-    await db.transaction(async (transaction) => {
-        console.log(reservation_time_limit+"\n"+updateJson)
-        try {
-            await db('reservations')
-                .where('reserved_at', '<', reservation_time_limit)
-                .select('*')
-                .transacting(transaction);
-            result.status = 'success';
-            await transaction.commit();
-        } catch (err) {
-            console.log(err);
-            result.status = 'failed';
-            await transaction.rollback();
-        }
-    });
-    return result;
+
+    const data  = await db('reservations')
+        .join('users', 'users.id', 'reservations.user_id')
+        .where('reserved_at', '>', reservation_time_limit)
+        .andWhere({status:'RESERVED'})
+        .select('*', 'reservations.id as id', 'users.id as user_id');
+    return data;
 };
 
-async function chargereservation(email){
-    stripe.customers.create({email})
-        .then(customer => console.log(customer.id))
-        .catch(error => console.error(error));
-    const charge = await stripe.charges.create({
-        amount: 100,
-        currency: 'usd',
-        source: 'tok_amex',
-        description: 'TEST',
-    });
-}
 
 
-module.exports={ cancelOutstandingReservations,checkOutstandingReservations}
+module.exports={ cancelReservationAndUpdateSpot,getOutstandingReservations}
---
+++ services/stripe/payment.js
@@ -0,0 +1,24 @@
+const stripe = require('./config');
+
+
+/**
+ * authorize payment by customer
+ * @param amount
+ * @param description
+ * @param stripeCustomer
+ * @param sourceId
+ * @returns {Promise<Stripe.Charge & {headers: {[p: string]: string}; lastResponse: {requestId: string; statusCode: number; apiVersion?: string; idempotencyKey?: string; stripeAccount?: string}}>}
+ */
+async function authorizeByCustomerAndSource(amount, description, stripeCustomerId, sourceId){
+    const charge = await stripe.charges.create({ // Return a charge object
+        amount: amount, // Unit: cents
+        currency: 'usd',
+        capture: false,
+        customer: stripeCustomerId,
+        source: sourceId,
+        description: description,
+    });
+    return charge;
+}
+
+module.exports={ authorizeByCustomerAndSource};
